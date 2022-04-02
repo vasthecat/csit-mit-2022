9af7ce1ec8057ac62c6d29db39c557ae2e9efe01
Евгений <example@example.com>
2022-04-02 20:03:08 +0400

    Добавлена итеративная реализация функции `int fib(int n)` вычисления n-го числа Фибоначчи.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <exception>
+#include <vector>
+
+int fib(int n) {
+    if (n <= 0) throw std::exception("you cannot do that");
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+
+    int a = 0, b = 1;
+    for (int i = 3; i <= n; i++) {
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
 
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
 }
