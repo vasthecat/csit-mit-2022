57f93ed23f8ed80dd31ebb76c9204f75df5060a4
Евгений <example@example.com>
2022-04-02 20:04:44 +0400

    Добавлена функция `void bubble(std::vector &v)` с реализацией cортировки пузырьком.

--- main.cpp
+++ main.cpp
@@ -16,9 +16,32 @@ int fib(int n) {
     return b;
 }
 
+template <typename T>
+void bubble(std::vector<T> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v.size() - 1; j++) {
+            if (v[j] > v[j + 1]) {
+                std::swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
     std::cout << fib(10) << std::endl;
+
+    std::vector<int> v = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+    for (int e : v) {
+        std::cout << e << ' ';
+    }
+    std::cout << std::endl;
+    bubble(v);
+    for (int e : v) {
+        std::cout << e << ' ';
+    }
+    std::cout << std::endl;
+
     return 0;
 }
