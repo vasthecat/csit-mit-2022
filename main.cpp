c3bc2a9cc1cdc9b17f0beaff4e683c5bfbd089d8
EgorChernishevskiy <100187552+EgorChernishevskiy@users.noreply.github.com>
2022-04-03 10:54:08 +0400

    В файл main.cpp добавлена итеративная реализация функции int fib(int n) вычисления n-го числа Фибоначчи.
    В функции main() должно выводиться 10-е число Фибоначчи, вычисленное с помощью этой функции.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int fib(int n) {
+    if (n <= 0) return -1;
+    if (n == 1) return 0;
+    if (n == 2) return -1;
+
+    int a = 0, b = 1;
+    for (int i = 3; i <= n; i++) {
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << std::endl;
+    cout << fib(10) << std::endl;
+
     return 0;
 }
