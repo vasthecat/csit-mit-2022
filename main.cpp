dad3168b13ca10ca2381c17a4f7da272ab4f349b
pr0suWANTED <prosuwanted@gmail.com>
2022-04-07 14:22:17 +0400

    Added a bubble sort function and a test case

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int fib(int n) {
     int p1 = 1;
@@ -12,9 +13,27 @@ int fib(int n) {
     return s;
 }
 
+void bubble_sort(std::vector<int> &v) {
+    bool swapped;
+    do {
+        swapped = false;
+        for (int i=0; i<v.size()-1; i++)
+            if (v[i] > v[i+1]) {
+                std::swap(v[i], v[i+1]);
+                swapped = true;
+            }
+    } while (swapped);
+}
+
 int main()
 {
+    std::vector<int> v{6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
     std::cout << "Hello, world!" << std::endl;
     std::cout << fib(10) << std::endl;
+    for (auto x : v) std::cout << x << " ";
+    std::cout << std::endl;
+    bubble_sort(v);
+    for (auto x : v) std::cout << x << " ";
+    std::cout << std::endl;
     return 0;
 }
