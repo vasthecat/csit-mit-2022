86ae9d6122d3ea908912e5d02c49d785deea64f4
pr0suWANTED <prosuwanted@gmail.com>
2022-04-07 14:18:24 +0400

    Added an iterative fibonacci function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+int fib(int n) {
+    int p1 = 1;
+    int p2 = 0;
+    int s = n - 1;
+    for (int i = 3; i <= n; i++) {
+        s = p2 + p1;
+        p2 = p1;
+        p1 = s;
+    }
+    return s;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
 }
