61639d8c19e016ccb5d03ef0b36f0c0bcc4bbca5
Masha Abu Saif <m.i.as006900@gmail.com>
2022-04-08 00:03:50 +0400

    Добавила сортировку пузырьком и вывод массива до и после сортировки

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 
 int fib(int n) {
@@ -8,8 +10,31 @@ int fib(int n) {
 }
 
 
+void output(std::vector<int> v) {
+    for (int i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+void bubble(std::vector<int>& v) {
+    output(v);
+    int n = v.size();
+    for (int i = 0; i < n - 1; ++i)
+        for (int j = n - 1; j > i; --j)
+            if (v[j - 1] > v[j])
+                std::swap(v[j - 1], v[j]);
+
+    output(v);
+}
+
+
 int main() {
+    std::vector<int> v = { 6, 2, 1, 4, 5, 6, 9, 12, 3, 1 };
+
     std::cout << "Hello, world!" << std::endl;
     std::cout << fib(10) << std::endl;
+    bubble(v);
     return 0;
 }
