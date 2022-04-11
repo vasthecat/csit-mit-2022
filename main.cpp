8242426592fdad66ea794cd324ed93b70913cb0e
Aleksandr <achekmarev@194-67-109-243.cloudvps.regruhosting.ru>
2022-04-11 11:39:15 +0300

    add function fib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, world!" << std::endl;
+using namespace std;
+
+void fib (int n){
+  int a = 0;
+  cout << a << endl;
+  if (n > 1){
+    int b = 1;
+    cout << b << endl;
+    int z = 0;
+    for(int i = 2; i < n; i++){
+      z = a + b;
+      cout << z << endl;
+      a = b;
+      b = z;
+    }
+    cout << z << endl;
+  }
+}
+
+int main(){
+    cout << "Hello, world!" << endl;
+    fib(10);
+    cout << endl;
     return 0;
 }
