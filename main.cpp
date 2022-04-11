5410af2aa6de3bf5423759852bef75d1f40f2638
Aleksandr <achekmarev@194-67-109-243.cloudvps.regruhosting.ru>
2022-04-11 11:45:46 +0300

    fixed function fib

--- main.cpp
+++ main.cpp
@@ -4,14 +4,11 @@ using namespace std;
 
 void fib (int n){
   int a = 0;
-  cout << a << endl;
   if (n > 1){
     int b = 1;
-    cout << b << endl;
     int z = 0;
     for(int i = 2; i < n; i++){
       z = a + b;
-      cout << z << endl;
       a = b;
       b = z;
     }
