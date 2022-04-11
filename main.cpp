f75782821a1fe0291ec1604e9a11a9207d53cffe
Olga Lokuta <olokuta20j@gmail.com>
2022-04-11 21:21:01 +0400

    Added function fib()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+int fib(int n) {
+    int a = 0;
+    int b = 1;
+    for (int i = 1; i < n; ++i) {
+        int temp = a;
+        a += b;
+        b = temp;
+    }
+    return a;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
 }
