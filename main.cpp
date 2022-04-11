ee7eb3f6c7cde07250221b19ac7bbb7ecbafcb5e
Anatolii Dergachev <anatolka18@yandex.ru>
2022-04-11 13:03:05 +0400

    Added bubble sorting and connected vectors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+void bubble(vector<int> &vec){
+  for (int i = 0; i < 9; i++) {
+    for (int j = 9; j > i; j--) {
+      if (vec[j - 1] > vec[j]) {
+	swap(vec[j - 1], vec[j]);
+      }
+    }
+  }
+  for(int i = 0; i < 10; i++){
+    cout << vec[i] << " ";
+  }
+  cout << endl;
+
+}
 void fib (int n){
   int a = 0;
   if (n > 1){
@@ -17,9 +32,15 @@ void fib (int n){
 
 int main()
 {
+    vector<int> vec = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
     cout << "Hello, world!" << endl;
     int a = 10;
     fib(a);
+    for(int i = 0; i < 10; i++){
+      cout << vec[i] << " ";
+    }
+    cout << endl;
+    bubble(vec);
     cout << endl;
     return 0;
 }
