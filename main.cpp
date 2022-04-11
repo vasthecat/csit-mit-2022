a6730576882a5beb2f4bf7f8f9fc3880ea5b959b
Anatolii Dergachev <anatolka18@yandex.ru>
2022-04-11 12:41:58 +0400

    Added a function that calculates fibonacci numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+using namespace std;
+
+void fib (int n){
+  int a = 0;
+  if (n > 1){
+    int b = 1;
+    int z = 0;
+    for(int i = 2; i < n; i++){
+      z = a + b;
+      a = b;
+      b = z;
+    }
+    cout << z << endl;
+  }
+}
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
+    int a = 10;
+    fib(a);
+    cout << endl;
     return 0;
 }
