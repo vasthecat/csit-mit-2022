1d5ca6e445ff8488c6c8c5aa8fece2a69baa5647
unknown <you@example.com>
2022-04-12 22:46:24 +0400

    Добавлена функция сортировки пузырьком

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 int fib(int n) {
 	int a = 0, b = 1;
 	for (int i = 0; i < n-1; ++i) {
@@ -8,9 +8,21 @@ int fib(int n) {
 		a = t;
 	}
 	return a;}
-int main()
-{
+void sort(std::vector<int> &v){
+    for (int i = 0; i < v.size(); i++) {
+    for (int j = 0; j < v.size() - 1; j++) {
+      if (v[j] > v[j + 1]) std::swap(v[j], v[j+1]);
+    }
+  }
+}
+int main(){
     std::cout << "Hello, world!" << std::endl;
     std::cout << fib(10) << std::endl;
+    std::vector<int> v = { 6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+    int t = v.size();
+    for (int i = 0; i < v.size(); i++) std::cout << v[i] << " "; 
+    sort(v);
+    std::cout << std::endl;
+    for (int i = 0; i <v.size(); i++) std::cout << v[i] << " "; 
     return 0;
 }
