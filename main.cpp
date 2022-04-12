87f417b2fe1ec87ef95be8c73033547aec8e6d15
landstring <landstring@gmail.com>
2022-04-12 23:07:30 +0400

    add function int fib(int n)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    else if (n < 4) {
+        return 1;
+    }
+    else {
+        return (fib(n - 1) + fib(n - 2));
+    }
+}
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
+    cout << fib(10) << endl;
     return 0;
 }
