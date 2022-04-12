4d80433635372955692bebdd8b58861d77675765
unknown <Lankin-03@mail.ru>
2022-04-12 18:02:13 +0400

    Добавил функцию fib(n) для вычисления n-ого числа Фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+int fib(int n)
+{
+    int a = 0;
+    int b = 1;
+    for(int i = 0; i < n-1; i++)
+    {
+        b = a + b;
+        a = b - a;
+    }
+    return a;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
 }
