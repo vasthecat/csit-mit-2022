f84508dd903e58b317794176e14bf8a989b5e002
BrovkoRoman <brovkora@rambler.ru>
2022-04-12 18:00:48 +0400

    Итеративное вычисление n-ого числа Фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
+int fib(int n)
+{
+    int a = 0, b = 1;
+
+    for(int i = 2; i <= n; i++)
+    {
+	int c = a + b;
+	a = b;
+	b = c;
+    }
+
+    return a;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
-    return 0;
+
+    cout << fib(10);
 }
