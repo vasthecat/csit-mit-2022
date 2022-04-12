4e6311e4996cee8a28b4ba133b47e4b4ff9f3f1e
MRossa157 <maxim.rossa@mail.ru>
2022-04-12 21:10:44 +0300

    Added fib(int n) function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+int fib(int n) {
+	int f1 = 0;
+	int f2 = 1;
+	int fn = 1;
+	if (n >= 1)
+		if (n >= 2)
+			for (int i = 0; i < n - 2; i++) {
+				fn = f1 + f2;
+				f1 = f2;
+				f2 = fn;
+			}
+
+	return fn;
+}
+
+
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10);
     return 0;
 }
