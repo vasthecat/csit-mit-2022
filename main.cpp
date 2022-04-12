ef9ba985d809ef21bfe00877e1b833bb5d79bec9
Anatoly Solomonov <solomonov010@mail.ru>
2022-04-12 15:52:27 +0400

    Added iterative Fibonacci function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+int fib(int n) {
+	int a = 0, b = 1;
+	for (int i = 0; i < n - 1; ++i) {
+		int t = b;
+		b += a;
+		a = t;
+	}
+	return a;
+}
+
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+	std::cout << "fib(13) = " << fib(13) << std::endl;
     return 0;
 }
