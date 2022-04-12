98bd7c484f2e9da5e931702d037041df2e198749
Andrey <vavilovo64@gmail.com>
2022-04-12 22:37:57 +0400

    Добавил итеративную функцию fib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+int fib(int x) {
+    int a = 0, b = 1, t;
+    for (int i = 1; i < x; ++i) {
+        t = b;
+        b += a;
+        a = t;
+    }
+    return a;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
 }
