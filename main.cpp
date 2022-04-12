78cd1205ebb1b48524d70651667bda6db4e4d074
Andrey <vavilovo64@gmail.com>
2022-04-12 22:40:45 +0400

    Добавил сортировку пузырьком

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int fib(int x) {
     int a = 0, b = 1, t;
@@ -10,9 +11,26 @@ int fib(int x) {
     return a;
 }
 
+void bubble(std::vector <int> &v)
+{
+    for (int i = 0; i < v.size() - 1; ++i) 
+    {
+        for (int j = 0; j < v.size() - 1 - i; ++j)
+        {
+            if (v[j] > v[j + 1]) std::swap(v[j], v[j + 1]);
+        }
+    }
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
     std::cout << fib(10) << std::endl;
+    std::vector <int> v = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+    for (auto i : v) std::cout << i << " ";
+    std::cout << std::endl;
+    bubble(v);
+    for (auto i : v) std::cout << i << " ";
+    std::cout << std::endl;
     return 0;
 }
