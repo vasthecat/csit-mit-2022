7e647d4777bf72e766ac0868a407643844977720
Kamil <Kamil>
2022-04-12 22:43:12 +0400

    Added fib function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+using namespace std;
+int fib(int n){
+    int a=0, b=1;
+    for (int i=2;i<n;++i){
+        int t = b;
+        b = a+b;
+        a= t;
+        
+    }
+    return b;
+}
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    cout<<fib(10);
     return 0;
 }
