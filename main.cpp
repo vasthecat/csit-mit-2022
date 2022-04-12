5aa247f427ad4244098ca9175168cbce08fc18c6
WorkManOne <arkhipov.ki@yandex.ru>
2022-04-13 00:42:33 +0400

    Added fib function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
+int fib(int n) {
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < n - 1; ++i) {
+        int t = a;
+        a = b;
+        b = b + t;
+    }
+
+    return a;
+}
+
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
+    cout << fib(10) << endl;
     return 0;
 }
