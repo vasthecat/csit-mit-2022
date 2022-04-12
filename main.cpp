4508abd3ac59334123698bc77ef3e7d835d0c41b
WorkManOne <arkhipov.ki@yandex.ru>
2022-04-13 00:49:27 +0400

    Added Bubble sort

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,7 +8,6 @@ int fib(int n) {
         return 0;
     if (n == 2)
         return 1;
-
     int a = 0;
     int b = 1;
     for (int i = 0; i < n - 1; ++i) {
@@ -15,13 +15,33 @@ int fib(int n) {
         a = b;
         b = b + t;
     }
-
     return a;
+}
+
+void bubbleSort(vector<int>& x) {
+    for (int i = 0; i < x.size() - 1; ++i) {
+        bool f = false;
+        for (int j = 0; j < x.size() - 1 - i; ++j)
+            if (x[j] > x[j + 1]) {
+                swap(x[j], x[j + 1]);
+                f = true;
+            }
+        if (!f)
+            break;
+    }
 }
 
 int main()
 {
     cout << "Hello, world!" << endl;
     cout << fib(10) << endl;
+    vector<int> x = { 6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+    for (vector<int>::iterator it = x.begin(); it != x.end(); it++)
+    cout << *it << " ";
+    cout << endl;
+    bubbleSort(x);
+    for (vector<int>::iterator it = x.begin(); it != x.end(); it++)
+    cout << *it << " ";
+    cout << endl;
     return 0;
 }
