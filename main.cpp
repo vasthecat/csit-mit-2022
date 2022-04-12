c594406fbd5748387dc638562cbed4b39bd48bdf
TheCoolEnot <dmutruiprutkov203040@gmail.com>
2022-04-12 21:17:59 +0400

    New file with iteration function fib(n)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+using namespace std;
+
+int fib(int n){
+    int x = 0, y = 1, z, r = 0;
+    if (n == 1) return 0;
+    else if (n == 2) return 1;
+    else {
+        for (int i = 2; i < n; ++i){
+            z = x + y;
+            r = z;
+            x = y;
+            y = z;
+        }
+        return r;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
+	cout << fib(10);
     return 0;
 }
