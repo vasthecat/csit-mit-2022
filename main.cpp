1d206bbc5d9fa7075d4f378361c4ba3200cef8cf
Kirill Kirnosov <kirx2@yandex.ru>
2022-04-12 23:02:58 +0400

    Added iterative Fibonacci function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+int fib(int n) {
+    int last = 0, next = 1;
+
+    for (int i = 3; i <= n; i++)
+        std::swap(last += next, next);
+
+    return (n>2)?next:n-1;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
+
     return 0;
 }
