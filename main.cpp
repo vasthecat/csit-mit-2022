9c88d09edf6aeade8c84dc70f25b0363cad44fb6
Kirill <grah154@rambler.ru>
2022-04-12 23:31:28 +0400

    added void bubble(std) function::vector<int> &v) with bubble sorting implementation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int fib(int n) {
     if (n <= 0) return -1;
@@ -15,11 +16,30 @@ int fib(int n) {
     return b;
 }
 
+void bubble(std::vector<int> &v) {
+    for (int i = 0; i != v.size(); ++i) {
+        for (int j = v.size() - 1; j != i; --j) {;
+            if (v[j - 1] > v[j]) 
+                std::swap(v[j - 1], v[j]);
+        }
+    }
+}
+
 
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
     std::cout << fib(10) << std::endl;
 
+    std::vector<int> vec = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+    for (int i = 0; i != vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    } 
+    std::cout << std::endl;
+    bubble(vec);
+    for (int i = 0; i != vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    } 
+    std::cout << std::endl;
     return 0;
 }
