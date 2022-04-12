72824652978c3f6667e8c1d6cf83cbf691c73b35
Егор <tlmiracle@yandex.ru>
2022-04-12 22:56:10 +0400

    добавил функцию для вычисления числа Фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-int main()
+using namespace std;
+
+//функция Фибоначчи
+int fib(int n)
+{
+    int a = 0;
+    int b = 1;
+    for(int i = 2; i <= n; i++){
+	int c = a + b;
+	a = b;
+	b = c;
+    }
+    return a;
+}
+
+void main()
 {
     std::cout << "Hello, world!" << std::endl;
-    return 0;
+
+    cout << fib(10);
 }
