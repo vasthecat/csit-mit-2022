4f9f72b62e0dddd81feb616937dbc4feabcb2adf
ElizabethBey <elizbey@yandex.ru>
2022-04-12 22:27:05 +0400

    Добавила итеративную функцию вычисления n-го числа Фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+int fib(int n)
+{
+    int a = 0, b = 1;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int temp = b;
+        b += a;
+        a = temp;
+    }
+    return a;
+}
+
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
+    cout << fib(10) << endl;
     return 0;
 }
