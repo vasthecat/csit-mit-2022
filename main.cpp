79fd4ab49f69247a6437e9f39c4f231b1e9854d5
ElizabethBey <elizbey@yandex.ru>
2022-04-12 22:35:59 +0400

    Добавила функцию сортировки вектора пузырьком

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void bubble(std::vector<int> &v)
+{
+    bool swapp = true;
+    while (swapp)
+    {
+        swapp = false;
+        for (size_t i = 0; i < v.size() - 1; i++)
+        {
+            if (v[i] > v[i + 1])
+            {
+                v[i] += v[i + 1];
+                v[i + 1] = v[i] - v[i + 1];
+                v[i] -= v[i + 1];
+                swapp = true;
+            }
+        }
+    }
+}
 
 int fib(int n)
 {
@@ -18,5 +38,15 @@ int main()
 {
     cout << "Hello, world!" << endl;
     cout << fib(10) << endl;
+    vector<int> v{6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+
+    for (vector<int>::iterator i = v.begin(); i != v.end(); i++)
+        cout << *i << " ";
+
+    bubble(v);
+
+    cout << endl;
+    for (vector<int>::iterator i = v.begin(); i != v.end(); i++)
+        cout << *i << " ";
     return 0;
 }
