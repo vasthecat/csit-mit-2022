bad55c57c4dcb2027751b5c89914127624fffb1b
egor <egor-rdd@mail.ru>
2022-04-12 23:01:59 +0400

    To a file main.cpp added an iterative implementation of the int fib(int n) function calculating the nth Fibonacci number. The main() function should output the 10th Fibonacci number calculated using this function;

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
+int fib() {
+    int n = 10;
+    int f1 = 0;
+    int f2 = 1;
+    int fn = 1;
+    if (n >= 1)
+        if (n >= 2)
+            for (int i = 0; i < n - 2; i++) {
+                fn = f1 + f2;
+                f1 = f2;
+                f2 = fn;
+            }
+
+    return fn;
+}
+
+
+
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+
+    cout << "Hello, world!" << endl;
+
+    cout << fib();
+
     return 0;
 }
