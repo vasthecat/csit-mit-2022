1d92b88d1483bef778734897064e197c1b2a8aae
Anatoly Solomonov <solomonov010@mail.ru>
2022-04-12 16:22:15 +0400

    Added function of bubble sorting with example

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int fib(int n) {
 	int a = 0, b = 1;
@@ -10,8 +11,21 @@ int fib(int n) {
 	return a;
 }
 
+void bubble(std::vector<int>& v) {
+	for (int i = 0; i < v.size() - 1; ++i)
+		for (int j = v.size() - 1; j > i; --j)
+			if (v[j - 1] > v[j])
+				std::swap(v[j - 1], v[j]);
+}
+
 int main()
 {
+	std::vector<int> vec = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+
 	std::cout << "fib(13) = " << fib(13) << std::endl;
+
+	for (int i : vec) std::cout << i << " ";
+	bubble(vec);
+	cout << endl; for (int i : vec) std::cout << i << " ";
     return 0;
 }
