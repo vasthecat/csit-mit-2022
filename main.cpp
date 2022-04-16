39fb70e50442e47e0e3b8028e4aadcca123632e6
unknown <shamistan2002@icloud.com>
2022-04-17 01:29:48 +0400

    The void bubble sort function has been added to the file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void bubble(std::vector<int>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec.size() - 1; j++) {
+            if (vec[j] > vec[j + 1])
+                swap(vec[j], vec[j + 1]);
+        }
+    }
+}
 
 int fib(int n) {
     int c, a = 0;
@@ -16,5 +26,15 @@ int fib(int n) {
 int main() {
     cout << "Hello, World!" << endl;
     cout << fib(10) << endl;
+    vector<int> vec = { 6, 2, 1, 4, 5, 6, 9, 12, 3, 1 };
+    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
+        cout << *iter << " ";
+    cout << endl;
+
+    bubble(vec);
+
+    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
+        cout << *iter << " ";
+    cout << endl;
     return 0;
 }
