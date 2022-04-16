5f7c9dc51d5fae04ee2116e7add78a9168a0672e
unknown <shamistan2002@icloud.com>
2022-04-17 01:16:56 +0400

    Task

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, world!" << std::endl;
+using namespace std;
+
+int fib(int n) {
+    int c, a = 0;
+    int b = 1;
+    for (int i = 3; i <= n; i++) {
+        c = b;
+        b = a + b;
+        a = c;
+    }
+    return b;
+}
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << fib(10) << endl;
     return 0;
 }
