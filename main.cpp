fa4012fdbbb26bd625a88702822fe477cab90dd8
stigmaAf <101147327+stigmaAf@users.noreply.github.com>
2022-04-24 03:06:56 +0400

    добавлена функция cортировки вектора пузырьком

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 int fib(int n){
     int a=0;
     int b=1;
@@ -11,9 +11,32 @@ int fib(int n){
     return a;
 }
 
+void bubble(std::vector<int> &v){
+    for(int i=0;i<v.size();i++){
+        for(int j=0;j<v.size()-1;j++){
+            if (v[j]>v[j+1]){
+                std::swap(v[j],v[j+1]);
+            }
+        }
+    }
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+
     std::cout << fib(10) << std::endl;
+
+    std::vector<int> v = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+    for(int i=0;i<v.size();i++){
+        std::cout<<v[i]<<" ";
+    }
+    std::cout<<std::endl;
+    bubble(v);
+    for(int i=0;i<v.size();i++){
+        std::cout<<v[i]<<" ";
+    }
+    std::cout<<std::endl;
+
     return 0;
 }
