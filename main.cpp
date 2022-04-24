d27d63023fc6c90d93a8c04ae00f35411003dde0
efimovand <100632223+efimovand@users.noreply.github.com>
2022-04-24 18:52:42 +0400

    Added bubble sort function for vector

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int fib(int n) {
 
@@ -10,11 +11,47 @@ int fib(int n) {
 
 }
 
+template <typename T>
+inline void swap( T & arg1, T & arg2)
+{
+    T temp = arg1;
+    arg1 = arg2;
+    arg2 = temp;
+};
+template <typename T>
+void bubble(std::vector<T> & vArray)
+{
+    for (int i = 0; i < vArray.size(); ++i)
+    {
+        for (int j = vArray.size()-1; j > i; --j)
+        {
+            if (vArray[i] > vArray[j])
+            {   swap( vArray[i], vArray[j]); }
+        }
+    }
+};
+
 int main()
 {
+    std::vector<int> a = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1}; // вектор с заданными числами
+
     std::cout << "Hello, world!" << std::endl;
 
     std::cout << fib(9) << std::endl; // 9, т.к. счет начинается с нуля
+
+    // вывод начального вектора на экран
+    for (std::vector<int>::iterator it = a.begin() ; it!=a.end() ; ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;
+
+    bubble(a);
+
+     // вывод итогового вектора на экран
+    for (std::vector<int>::iterator it = a.begin() ; it!=a.end() ; ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;
 
     return 0;
 }
