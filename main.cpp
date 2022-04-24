d2b13e27084c883b7562514d3e79621a063fd54d
efimovand <100632223+efimovand@users.noreply.github.com>
2022-04-24 18:38:05 +0400

    Added recursive Fibonacci function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+int fib(int n) {
+
+    if (n == 0)
+        return 0; // базовый случай (условие завершения)
+    if (n == 1)
+        return 1; // базовый случай (условие завершения)
+    return fib(n-1) + fib(n-2);
+
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+
+    std::cout << fib(9) << std::endl; // 9, т.к. счет начинается с нуля
+
     return 0;
 }
