02b9cbf462e6e6f6f569a68f3b03015a311afccb
ahmedalany7 <hazobazo2002@gmail.com>
2022-05-30 22:32:06 +0400

    Iterative fib function added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+int fib(int n) {
+    int last = 0;
+    int slast = 0;
+    int a = 0;
+    for (int i = 0; i < n; i++) {
+        a = last + slast;
+        if (a == 0) last = 1;
+        slast = last;
+        last = a;
+    }
+    return a;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
 }
