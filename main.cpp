2b1dfe3ece530fe8d1019ac0e14c7d91757c5ead
Maxim Ivanov <max2206.2019@yandex.ru>
2022-06-07 15:37:46 +0400

    Added fib function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+int fib(int n){
+    if (n == 1) return 0;
+    else if (n == 2) return 1;
+    return fib(n-2) + fib(n-1);
+}
+
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
 }
