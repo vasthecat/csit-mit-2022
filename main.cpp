dc203d199b801cf4f29a10f47428b328d0102b74
BlacksmithsHammer <denis.fatyushckin2019@yandex.ru>
2022-06-08 00:20:57 +0400

    An iterative function for implementing int fib(int n) of complexity of the nth Fibonacci number has been added to the main.cpp file.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+int fib(int n){                                             //calculating the fibonacci number
+    int tmp, n_1 = 0, n_2 = 1;
+    while (n-- > 1){
+        tmp = n_1;
+        n_1 = n_2;
+        n_2 = n_1 + tmp;
+    }
+    return n_1;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+
+    std::cout << fib(10) << std::endl;
+    
     return 0;
 }
