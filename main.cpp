fb73449ad2267a0b7ef009009efffd64232058c0
OneCatGamer <kekaterinushkin@mail.ru>
2022-06-08 17:44:27 +0300

    Added fib function for calculate fibonacci number

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int fib(int x)
+{
+    int a = 0, b = 1, q;
+    for (int i = 1; i < x; ++i) {
+        q = b;
+        b += a;
+        a = q;
+    }
+    return a;
+}
 
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+
+    cout << fib(10) << endl;
+
+    vector < int> v = { 6, 2, 1, 4, 5, 6, 9, 12, 3, 1 };
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << ' ';
+
+    cout << endl;
     return 0;
 }
