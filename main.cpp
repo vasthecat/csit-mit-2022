b4f9f59fdea732c72a595238dd17b0754d3f2ec5
ByThePainISeeInOthers <alexander29072003@gmail.com>
2022-06-08 18:21:32 +0400

    add fib(n)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, world!" << std::endl;
+using namespace std;
+int fib(int n) {
+    int a = 0, b = 1;
+    for (int i = 2; i < n; ++i) {
+        b += a;
+        a = b - a;
+    }
+    cout << b << endl;
+}
+
+int main() {
+    fib(10);
     return 0;
 }
