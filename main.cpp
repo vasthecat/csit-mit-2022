9ac82be1e0d3f15f6540c8e8f05dd73e111e06d3
ByThePainISeeInOthers <alexander29072003@gmail.com>
2022-06-08 18:45:21 +0400

    add bubble sort

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 int fib(int n) {
@@ -10,7 +11,35 @@ int fib(int n) {
     cout << b << endl;
 }
 
+void bubble(vector<int> &v) {
+    bool f = false;
+    while (!f) {
+        f = true;
+        for (int i = 0; i < v.size() - 1; ++i) {
+            if (v[i] > v[i + 1]) {
+                swap(v[i], v[i + 1]);
+                f = false;
+            }
+        }
+    }
+}
+
 int main() {
+    cout << "Hello, World!" << endl;
+
     fib(10);
+
+    vector<int> v = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+
+    for (int i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    bubble(v);
+
+    for (int i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
     return 0;
 }
