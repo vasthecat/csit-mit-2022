5267424acee20a9ffe0fdcdf3c50140feeda477e
OneCatGamer <kekaterinushkin@mail.ru>
2022-06-08 17:48:19 +0300

    Added bubble sort

--- main.cpp
+++ main.cpp
@@ -13,6 +13,15 @@ int fib(int x)
     return a;
 }
 
+void bubble(vector <int>& v)
+{
+    int n = v.size();
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - 1 - i; j++)
+            if (v[j] > v[j + 1])
+                swap(v[j], v[j + 1]);
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
@@ -24,5 +33,8 @@ int main()
         cout << v[i] << ' ';
 
     cout << endl;
+    bubble(v);
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << ' ';
     return 0;
 }
