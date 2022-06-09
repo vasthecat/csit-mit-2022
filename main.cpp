7ce526e719820a8f4074e4dd2ebf438a9e966086
gadievrm <guvho01@gmail.com>
2022-06-09 21:31:06 +0400

    Добавлена функция сортировки пузырьком и её демонстрация

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int fib(int n)
@@ -14,10 +15,30 @@ int fib(int n)
     return a;
 }
 
+void bubble(std::vector<int> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v.size(); j++) {
+            if (v[j] > v[j+1]) {
+                swap(v[j], v[j+1]);
+            }
+        }
+    }
+}
+
 
 int main()
 {
+    std::vector<int> vec = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
     cout << "Hello, world!" << endl;
     cout << fib(10) << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+    bubble(vec);
+    for (int i = 0; i < 10; i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
