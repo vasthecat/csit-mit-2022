71c87c4538dafea869854d0ee83fbbc1765a3725
Alotgrey <egor.kozh@mail.ru>
2022-06-09 20:35:03 +0400

    ADDING FIBONACCI FUNCTION AND PRINTING OT 10

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+int fib(int n) {
+	int f1 = 0;
+	int f2 = 1;
+	int fn = 1;
+	if (n >= 1)
+		if (n >= 2)
+			for (int i = 0; i < n - 2; i++) {
+				fn = f1 + f2;
+				f1 = f2;
+				f2 = fn;
+			}
+
+	return fn;
+}
+
+
+
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+
+	cout << "Hello, world!" << endl;
+
+	cout << fib(10);
+
 }
