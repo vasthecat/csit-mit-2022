9b8756b3afe644d777dd34e56d9d5eaf3c577448
aivanov <aivanov@mit.cloudvps.regruhosting.ru>
2022-06-10 10:24:16 +0300

    added fib function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include<iostream>
+
+
+void fib(int n){
+    int e1=0, e2=1;
+    bool f=true;
+
+    for (int i=0; i<n-2; i++){
+        if(f){
+            e1 += e2;
+        } else{
+            e2 += e1;
+        }
+        f = !f;
+    }
+    std::cout << (f ? e2 : e1) << "\n";
+}
+
 
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+
+    fib(10);
+
     return 0;
+
 }
