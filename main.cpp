e59c8c0f9c3ec1fc660fb60c8425b9ce0f636694
danko328 <daniil-kondrashov-rabotaet@yandex.ru>
2022-06-10 09:47:18 +0400

    добавил функцию иттеративного вычисления числа Фиббоначчи

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+int fib(int n)
+{
+	int temp, a = 0, b = 1;
+	if(n == 0)
+		return a;
+	else if(n == 1)
+		return b;
+	else
+	{
+		for(int i = 0; i < n-1; ++i)
+		{
+			temp = b;
+			b += a;
+			a = temp;
+		}
+		return a;
+	}
+}
+
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+	std::cout << "Hello, world!" << std::endl;
+	std::cout << fib(10) << std::endl;
+	return 0;
 }
