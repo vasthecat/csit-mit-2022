1107ad15a49e7df074154582a74f68ecb5636f09
danko328 <daniil-kondrashov-rabotaet@yandex.ru>
2022-06-10 10:04:11 +0400

    добавил функцию сортировки пузырьком

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <iostream>
+#include <vector>
+
+void bubble(std::vector<int> &v)
+{
+    while(true)
+    {
+        int count = 0;
+        for (int i = 0; i < v.size() - 1; ++i)
+        {
+            if(v[i] > v[i + 1])
+            {
+                std::swap(v[i], v[i+1]);
+                ++count;
+            }
+        }
+        if(count == 0)
+        {
+            break;
+        }
+    }    
+}
 
 int fib(int n)
 {
@@ -23,5 +45,13 @@ int main()
 {
 	std::cout << "Hello, world!" << std::endl;
 	std::cout << fib(10) << std::endl;
+    std::vector<int>v{6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
+    for (auto it = v.begin(); it != v.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+    bubble(v);
+    for (auto it = v.begin(); it != v.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl; 
 	return 0;
 }
