888b4438aebd1ddf058295340f220aade6677f74
aivanov <aivanov@mit.cloudvps.regruhosting.ru>
2022-06-10 10:29:59 +0300

    added bubble sort

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 
 void fib(int n){
@@ -17,11 +18,34 @@ void fib(int n){
 }
 
 
+void bubble(std::vector<int> &v)
+{
+    int i, j;
+    for (i = 0; i < v.size()-1; i++)
+        for (j = 0; j < v.size() - i - 1; j++)
+            if (v[j] > v[j + 1])
+                std::swap(v[j], v[j + 1]);
+}
+
+
 int main()
 {
+    std::vector<int> v = {6, 2, 1, 4, 5, 6, 9, 12, 3, 1};
     std::cout << "Hello, world!" << std::endl;
 
     fib(10);
+
+    for (std::vector<int>::iterator it=v.begin(); it!=v.end(); it++){
+        std::cout << *it << " ";
+    }
+    std::cout << "\n";
+
+    bubble(v);
+
+    for (std::vector<int>::iterator it=v.begin(); it!=v.end(); it++){
+        std::cout << *it <<  " ";
+    }
+    std::cout << "\n";
 
     return 0;
 
