c6567a9de0cda11019d9ec8c1c27701d7a00e309
ogurets667 <dangerousguy1337@gmail.com>
2022-11-03 21:05:49 +0400

    Добавлена функция fib(n)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+int fib(int n) {
+    int a = 0;
+    int b = 1;
+    int c;
+
+    for (int i = 1; i < n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+
+    std::cout << fib(10) << std::endl;
+
     return 0;
 }
